e1852fcdeb91f49360f1f92a0fa3b5312abd1bae
Lyraa-05 <20106693@mail.wit.ie>
2025-12-22 02:35:43 +0000

    Updated myreviewspage, similar to taskspage

--- react-movies/src/pages/myReviewsPage.jsx
+++ react-movies/src/pages/myReviewsPage.jsx
@@ -1,21 +1,70 @@
+import { useState, useEffect, useContext } from "react";
+import ReviewForm from "../components/reviewForm";
+import { getMyReviews, addReview, deleteReview } from "../api/reviews-api";
+import { AuthContext } from "../contexts/authContext";
+
 const MyReviewsPage = () => {
   const { token } = useContext(AuthContext);
 
-  const { data, isLoading } = useQuery({
-    queryKey: ["myReviews"],
-    queryFn: () => getMyReviews(token),
+  const [reviewState, setReviewState] = useState({ reviews: [] });
+  const [formState, setFormState] = useState({
+    movieId: "",
+    review: "",
+    rating: 3,
   });
 
-  if (isLoading) return <Spinner />;
+  useEffect(() => {
+    getMyReviews(token).then(reviews => {
+      setReviewState({ reviews });
+    });
+  }, [token]);
+
+  const formChangeHandler = (event) => {
+    setFormState({
+      ...formState,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const formSubmitHandler = async (event) => {
+    event.preventDefault();
+
+    const newReview = await addReview(formState, token);
+
+    setReviewState({
+      reviews: [...reviewState.reviews, newReview],
+    });
+  };
+
+  const deleteHandler = async (index) => {
+    const reviews = [...reviewState.reviews];
+    const id = reviews[index]._id;
+
+    await deleteReview(id, token);
+    reviews.splice(index, 1);
+
+    setReviewState({ reviews });
+  };
 
   return (
     <>
       <h2>My Reviews</h2>
-      {data.map(r => (
-        <p key={r._id}>
-          Movie ID: {r.movieId} — Rating: {r.rating}
-        </p>
+
+      {reviewState.reviews.map((r, index) => (
+        <div key={r._id}>
+          <p>
+            Movie ID: {r.movieId} — Rating: {r.rating}
+          </p>
+          <button onClick={() => deleteHandler(index)}>Delete</button>
+        </div>
       ))}
+
+      <ReviewForm
+        submit={formSubmitHandler}
+        change={formChangeHandler}
+      />
     </>
   );
 };
+
+export default MyReviewsPage;
